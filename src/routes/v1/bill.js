b77a44e261778904a34edf55ac43eee1ab993332
Pratik <pratiknimbole27@gmail.com>
2025-12-02 16:10:50 +0530

    SPPAI-269

--- src/routes/v1/bill.js
+++ src/routes/v1/bill.js
@@ -56,7 +56,7 @@ router.delete(
 
 // GET case-wise bill details
 router.get(
-  "/bill/:companyId/open/:year/:month/case/:caseId",
+  "/open/:companyId/:year/:month/case/:caseId",
   getBillCaseDetails
 );
 
